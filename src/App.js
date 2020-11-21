a7eb507189be6c6529224d2cf00b33170018d8e9
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-21 11:01:12 +0100

    Query with graphql variables

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ const axiosGitHubGraphQL = axios.create({
 
 const TITLE = "React GraphQl Github Client";
 
-const getIssuesOfRepositoryQuery = (organization, repository) => `
-  {
-    organization(login: "${organization}") {
+const GET_ISSUES_OF_REPOSITORY = `
+  query ($organization: String!, $repository: String!){
+    organization(login: $organization) {
       name
       url
-      repository(name: "${repository}") {
+      repository(name: $repository) {
         name
         url
         issues(last:5){
@@ -35,7 +35,8 @@ const getIssuesOfRepositoryQuery = (organization, repository) => `
 const getIssuesOfRepository = (path) => {
   const [organization, repository] = path.split("/");
   return axiosGitHubGraphQL.post("", {
-    query: getIssuesOfRepositoryQuery(organization, repository),
+    query: GET_ISSUES_OF_REPOSITORY,
+    variables: { organization, repository },
   });
 };
 
